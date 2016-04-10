808b3ed42315fa877775fb869ab8d579c90e0c59
Christian Shtarkov <cshtarkov@gmail.com>
2016-04-10 21:36:12 +0100

    Fix GPL formatting

--- emacs-lzw.el
+++ emacs-lzw.el
@@ -8,9 +8,9 @@
 ;; This file is part of emacs-lzw.
 
 ;; emacs-lzw is free software: you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free
-;; Software Foundation, either version 3 of the License, or (at your
-;;                                                              option) any later version.
+;; the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
 
 ;; emacs-lzw is distributed in the hope that it will be useful, but WITHOUT ANY
 ;; WARRANTY; without even the implied warranty of MERCHANTABILITY or
